16c9a4e4c748e6ddd31800263ba5d3d1aac6a383
sdelon <delon.steph@gmail.com>
2021-07-13 15:52:18 +0200

    wip

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -11,9 +11,9 @@
     <div class="lg:w-2/3 px-2 lg:pr-10 mt-20 order-1 lg:order-none">
       <p class="text-gray-light uppercase text-sm tracking-wide pb-3">{PrismicDom.RichText.asText(accueil.data.sous_titre)}</p>
       <div class="relative">
-          <h1 class="text-5xl font-black text-gray-800 max-w-3xl mx-auto">{extractMotUnderline(accueil.data.titre_principal[0].text, accueil.data.mot_underline)} <span class="relative">
+          <h1 class="text-3xl sm:text-5xl font-black text-gray-800 max-w-3xl mx-auto">{extractMotUnderline(accueil.data.titre_principal[0].text, accueil.data.mot_underline)} <span class="relative">
               {accueil.data.mot_underline}
-              <div class="absolute -bottom-7 right-0 -mr-10">
+              <div class="absolute -bottom-9 sm:-bottom-7 right-3 sm:right-0 -mr-10">
                   <svg class="svg-shadow" width="176" height="78" viewBox="0 0 176 78" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M4.16866 49.7734C49.6249 56.8271 214.397 64.3995 159.471 26.2365" stroke="#F9F871" stroke-width="6" stroke-linecap="round"/>
                   </svg>                    
--- src/lib/Projet.svelte
+++ src/lib/Projet.svelte
@@ -15,7 +15,7 @@
 {#if isModalClicked}
 <Modal titre_projet={PrismicDom.RichText.asText(slice.titre_projet)}>
     <div>
-        <div class="bg-gray-200 flex justify-between items-center p-12">
+        <div class="bg-gray-200 flex justify-between items-center sm:p-12">
             <p class="text-5xl font-black text-gray-800">{PrismicDom.RichText.asText(slice.titre_projet)}</p>
             <svg on:click={() => isModalClicked = !isModalClicked} class="w-10 h-10 cursor-pointer text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
         </div>
@@ -38,7 +38,7 @@
         height={slice.image.dimensions.height}
         styles="w-full h-full object-cover"/>
     </div>
-    <div class="bg-gray-200 p-20 relative">
+    <div class="bg-gray-200 p-5 sm:p-20 relative">
         <button
         on:mouseenter={() => hovering = true}
         on:mouseleave={() => hovering = false}
@@ -52,12 +52,16 @@
             {/if}
             {/key}
         </button>
-        <div class="leading-loose h-full flex justify-center items-center">
+        <div class="leading-loose h-full flex justify-center items-center mt-8 sm:mt-0">
             <div>
-                <h3 class="font-black text-gray-800 text-6xl pb-1">{PrismicDom.RichText.asText(slice.titre_projet)}</h3>
+                <h3 class="font-black text-gray-800 sm:text-6xl pb-1">{PrismicDom.RichText.asText(slice.titre_projet)}</h3>
                 <h4 class="uppercase font-light text-gray-600 tracking-wide pb-6">{PrismicDom.RichText.asText(slice.categories_projet)}</h4>
                 <div class="text-gray-800">{@html PrismicDom.RichText.asHtml(slice.description_projet)}</div>
-                <a class="uppercase font-black text-gray-600 tracking-widest text-sm inline-block mt-10" href="{slice.lien_projet.url}" target="blank">Découvrir le projet en ligne</a>
+                <a class="relative uppercase font-bold hover:font-black text-gray-600 tracking-widest text-sm inline-block mt-10 mb-8 sm:mb-0" href="{slice.lien_projet.url}" target="blank">Découvrir le projet en ligne
+                    <svg class="absolute top-3 right-2 sm:-right-3 drop-shadow-lg" width="84" height="29" viewBox="0 0 84 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3.66653 17.8419C10.2741 20.164 28.0441 24.0206 46.2638 20.8697C69.0385 16.9312 75.1828 12.1579 80.507 8.71565" stroke="#F9F871" stroke-width="6" stroke-linecap="round"/>
+                    </svg>                   
+                </a>
             </div>
         </div>
     </div>
