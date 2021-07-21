531d72b62f53d9c8f7917aba24563c087428038a
sdelon <delon.steph@gmail.com>
2021-07-21 08:12:06 +0200

    add scroll anchor effect

--- src/lib/UI/NavList.svelte
+++ src/lib/UI/NavList.svelte
@@ -1,27 +1,24 @@
 <script>
     import { page } from '$app/stores'
-    // import { gsap } from 'gsap'
-    // import { ScrollToPlugin } from '../../../node_modules/gsap/dist/ScrollToPlugin'
+    import { gsap } from 'gsap'
+    import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin.js'
 
     export let styles, item_styles = "", link_styles = "", isMobile = false
+    
+    gsap.registerPlugin(ScrollToPlugin)
 
-    // gsap.registerPlugin(ScrollToPlugin)
-
-    // const scrollToAnchor = (node, params) => {
-
-    // function goToAnchor(e) {
-    //     if(e) e.preventDefault()
-    //         gsap.to(window, { duration: 1, scrollTo: `#${params}`})
-    //     }
-
-    //     node.addEventListener('click', goToAnchor)
-
-    //     return {
-    //         onDestroy() {
-    //             node.removeEventListener('click', goToAnchor)
-    //         }
-    //     }
-    // }
+    const scrollToAnchor = (node, params) => {
+    function goToAnchor(e) {
+        if(e) e.preventDefault()
+            gsap.to(window, { duration: 1, scrollTo: `#${params}`})
+        }
+        node.addEventListener('click', goToAnchor)
+        return {
+            onDestroy() {
+                node.removeEventListener('click', goToAnchor)
+            }
+        }
+    }
 </script>
 
 <style>
@@ -39,8 +36,8 @@
     <li class="list-none {item_styles}">
         {#if $page.path === '/'}
         {#if isMobile}<slot name="services"></slot>{/if}
-        <!-- <a use:scrollToAnchor={'services'} class="{link_styles}" href="/#services">Services</a> -->
-        <a class="{link_styles}" href="/#services">Services</a>
+        <a use:scrollToAnchor={'services'} class="{link_styles}" href="/#services">Services</a>
+        <!-- <a class="{link_styles}" href="/#services">Services</a> -->
         {:else}
         {#if isMobile}<slot name="services"></slot>{/if}
         <a class="{link_styles}" href="/#services">Services</a>
@@ -49,8 +46,8 @@
     <li class="list-none {item_styles}">
         {#if $page.path === '/'}
         {#if isMobile}<slot name="projets"></slot>{/if}
-        <!-- <a use:scrollToAnchor={'projets'} class="{link_styles}" href="/#projets">Projets</a> -->
-        <a class="{link_styles}" href="/#projets">Projets</a>
+        <a use:scrollToAnchor={'projets'} class="{link_styles}" href="/#projets">Projets</a>
+        <!-- <a class="{link_styles}" href="/#projets">Projets</a> -->
         {:else}
         {#if isMobile}<slot name="projets"></slot>{/if}
         <a class="{link_styles}" href="/#projets">Projets</a>
