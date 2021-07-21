de6c3354aa81f15f2e8732e314fd30d3144584d6
sdelon <delon.steph@gmail.com>
2021-07-21 15:25:40 +0200

    perf image component (srcset, placeholder)

--- src/lib/UI/Img.svelte
+++ src/lib/UI/Img.svelte
@@ -15,11 +15,30 @@
     const placeholder = src => {
         return `${src}&blur=200&px=16`
     }
+
+    const display_srcset = (src, arr) => {
+        let result = []
+        const image_src = src.split('?')[0]
+        let single_format = arr.map(width => {
+            return `${image_src}?dpr=3&q=80&w=${width} ${width}w`
+        })
+
+        result.push(single_format)
+        return result.join(', ')
+    }
 </script>
 
 <div class="{container_styles}" use:inview={options} on:change={handleChange}>
     {#if isInView}
-        <img src={src} loading="lazy" {width} {height} alt={alt} class="{styles}">
+        <img 
+        src={src}  
+        sizes="(min-width: 48em) 50vw, 100vw"
+        srcset={display_srcset(src, [100,200,300])}
+        loading="lazy" 
+        {width} 
+        {height} 
+        alt={alt} 
+        class="{styles}">
     {:else}
         <img src={placeholder(src)} loading="lazy" {width} {height} alt={alt} class="{styles}">
     {/if}
