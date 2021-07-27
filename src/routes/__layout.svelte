999ed6bcd38bf30de936a80e81eff07c2e6eebcb
sdelon <delon.steph@gmail.com>
2021-07-27 09:10:50 +0200

    wip

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,20 +4,6 @@
   import Footer from '$lib/Footer.svelte'
 </script>
 
-<svelte:head>
-  <script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.1.3/src/glowCookies.min.js"></script>
-  <script>
-      glowCookies.start('fr', { 
-          style: 1,
-          analytics: 'G-TMX3F24LXV', 
-          policyLink: 'https://policies.google.com/technologies/cookies?hl=fr-FR',
-          hideAfterClick: true,
-          acceptBtnBackground: '#0C3E3B',
-          rejectBtnBackground: '#F3F3F3'
-      });
-  </script>
-</svelte:head>
-
 <Nav />
 <main class="bg-gray-bg min-h-screen">
     <slot />
