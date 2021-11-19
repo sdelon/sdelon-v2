6ebc995bd0abd532d1e922fc0594a12d5085f463
sdelon <delon.steph@gmail.com>
2021-11-19 07:42:08 +0100

    ga axeptio

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,44 +2,10 @@
   import "../app.postcss"
   import Nav from '$lib/Nav.svelte'
   import Footer from '$lib/Footer.svelte'
-  import CookieConsent from '$lib/CookieConsent.svelte'
 
-	let choices = {
-		tracking: false,
-		marketing: false
-	}
-	let color_primary = "#0D3E3A"
-	let color_secondary = "#F6F7F9"
-  let is_analytics = false
-
-  function initAnalytics() {
-    is_analytics = !is_analytics
-
-    if(is_analytics) {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('consent', 'default', {
-          'ad_storage': 'denied',
-          'analytics_storage': 'granted'
-      });
-      gtag('js', new Date());
-      gtag('config', 'G-TMX3F24LXV');
-    } else {
-      window['ga-disable-G-TMX3F24LXV'] = true;
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('consent', 'default', {
-          'ad_storage': 'denied',
-          'analytics_storage': 'denied'
-      });
-      gtag('js', new Date());
-      gtag('config', 'G-TMX3F24LXV');
-    }
-  }
 </script>
 
 <svelte:head>
-  <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-TMX3F24LXV"></script> -->
   <script>
       window.axeptioSettings = {
         clientId: "6139c090defe3c3e027781ae",
@@ -50,6 +16,29 @@
         e.async = true; e.src = "//static.axept.io/sdk.js";
         t.parentNode.insertBefore(e, t);
       })(document, "script");
+    
+    function launchGA(){
+      var el = document.createElement('script');
+      el.setAttribute('type', 'text/javascript');
+      el.setAttribute('async', true);
+      el.setAttribute('src', '//www.googletagmanager.com/gtag/js?id=G-TMX3F24LXV');
+      document.body.append(el); window.dataLayer = window.dataLayer || [];
+
+      function gtag(){ dataLayer.push(arguments); }
+      
+      gtag('js', new Date());
+      gtag('config', 'UA-234217-21', { 'anonymize_ip': true });
+      gtag('config', 'UA-234217-21', {'link_attribution': true});
+    }
+    // La fonction qui relie Axeptio au lancement du cookie Google Analytics
+    void 0 === window._axcb && (window._axcb = []);
+    window._axcb.push(function(axeptio) {
+        axeptio.on("cookies:complete", function(choices) {
+        if(choices.google_analytics) {
+          launchGA();
+          }
+        });
+    });
     </script>
 </svelte:head>
 
@@ -58,10 +47,3 @@
     <slot />
 </main>
 <Footer />
-<!-- <CookieConsent 
-{color_primary} 
-{color_secondary} 
-cookieName="sdelon_cookie_consent" 
-description="En autorisant l'utilisation des cookies - petit fichier texte déposé sur votre ordinateur lors de votre visite sur le site - vous acceptez le dépôt et la lecture de cookies et l'utilisation de technologies de suivi nécessaires à leur bon fonctionnement." 
-{choices} 
-on:analytics={initAnalytics} /> -->
